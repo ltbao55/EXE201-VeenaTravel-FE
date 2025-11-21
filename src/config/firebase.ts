7f7768d5acffc520b456aa79e1f7329d2764b2ec
Dan-G5 <dangtran2652004@gmail.com>
2025-11-21 18:28:54 +0700

    chore: add vercel rewrite and move firebase config to env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,13 +6,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA_vK9pcZQ7mTj6czOaoqtuswt3bXB8w84",
-  authDomain: "veenatravel-448dd.firebaseapp.com",
-  projectId: "veenatravel-448dd",
-  storageBucket: "veenatravel-448dd.firebasestorage.app",
-  messagingSenderId: "555989091333",
-  appId: "1:555989091333:web:2e50260b67f8f637adc86c",
-  measurementId: "G-34ZB5742VM",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "",
 };
 
 // Initialize Firebase
